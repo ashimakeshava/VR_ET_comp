72953ec8b5a45c546d683a2216d1983817271aec
Linus T <linus-tiemann@live.de>
2020-10-07 23:21:33 +0200

    added some testing functions

--- Assets/Scripts/Functionality/HeadTrackingSpace.cs
+++ Assets/Scripts/Functionality/HeadTrackingSpace.cs
@@ -7,11 +7,15 @@ public class HeadTrackingSpace : MonoBehaviour
 {
     public GameObject OrientationCross;
 
+    public GameObject FixationCross;
+
     public GameObject YawSetup;
 
     public GameObject PitchSetup;
 
     public GameObject RollSetup;
+
+    private bool CalibrationStatus;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,9 +27,56 @@ public class HeadTrackingSpace : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.A))
         {
-            OrientationCross.transform.GetChild(0).gameObject.SetActive(false);
-            OrientationCross.transform.GetChild(1).gameObject.SetActive(false);
+            if (CalibrationStatus)
+            {
+                SetExperimentStatus();
+                CalibrationStatus = false;
+            }
+            else
+            {
+                SetCalibrationStatus();
+                CalibrationStatus = true;
+            }
         }
+        
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (YawSetup.activeInHierarchy)
+            {
+                YawSetup.SetActive(false);
+            }
+            else
+            {
+                YawSetup.SetActive(true);
+            }
+        }
+        
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (PitchSetup.activeInHierarchy)
+            {
+                PitchSetup.SetActive(false);
+            }
+            else
+            {
+                PitchSetup.SetActive(true);
+            }
+        }
+        
+        
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            if (RollSetup.activeInHierarchy)
+            {
+                RollSetup.SetActive(false);
+            }
+            else
+            {
+                RollSetup.SetActive(true);
+            }
+        }
+          
         
         if (Input.GetKeyDown(KeyCode.Space))
         {
@@ -43,5 +94,23 @@ public class HeadTrackingSpace : MonoBehaviour
             RollSetup.transform.position = new Vector3(RollSetup.transform.position.x,yPos, RollSetup.transform.position.z);
             OrientationCross.transform.position =  new Vector3(OrientationCross.transform.position.x,yPos, OrientationCross.transform.position.z);
         }
+
+
+
+       
+    }
+    
+    private void SetCalibrationStatus()
+    {
+        OrientationCross.transform.GetChild(0).gameObject.SetActive(true);
+        OrientationCross.transform.GetChild(1).gameObject.SetActive(true);
+        FixationCross.SetActive(true);
+    }
+
+    private void SetExperimentStatus()
+    {
+        FixationCross.SetActive(false);
+        OrientationCross.transform.GetChild(0).gameObject.SetActive(false);
+        OrientationCross.transform.GetChild(1).gameObject.SetActive(false);
     }
 }
