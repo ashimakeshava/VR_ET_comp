a1ba538ee687850bd121f6e9d535eb72455f4c76
Linus T <linus-tiemann@live.de>
2020-10-07 23:06:41 +0200

    added a Basic Headtracking Space
    the headtracking is alligned towards a target cross, Roll Pitch and Yaw get aligned with the height of the head. the desired steps are pitch and Yaw to a 16 and 32 degree , and Roll in 10 and 15 degree.
    
    There is currently no implemented functions. Probably we use similar functions as in LoopAR. Probably we need to find a solution if the participant should allign itself or this is done with help of the experimenter.

---
+++ Assets/Scripts/Functionality/HeadTrackingSpace.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class HeadTrackingSpace : MonoBehaviour
+{
+    public GameObject OrientationCross;
+
+    public GameObject YawSetup;
+
+    public GameObject PitchSetup;
+
+    public GameObject RollSetup;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            OrientationCross.transform.GetChild(0).gameObject.SetActive(false);
+            OrientationCross.transform.GetChild(1).gameObject.SetActive(false);
+        }
+        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("plop");
+
+            //Todd Wassen miracle solution for alligning the WorldSpace to the Camera
+            Valve.VR.OpenVR.System.ResetSeatedZeroPose();
+            Valve.VR.OpenVR.Compositor.SetTrackingSpace(
+                Valve.VR.ETrackingUniverseOrigin.TrackingUniverseSeated);
+
+            float yPos = Camera.main.transform.position.y;
+
+            YawSetup.transform.position = new Vector3(YawSetup.transform.position.x,yPos, YawSetup.transform.position.z);
+            PitchSetup.transform.position = new Vector3(PitchSetup.transform.position.x,yPos, PitchSetup.transform.position.z);
+            RollSetup.transform.position = new Vector3(RollSetup.transform.position.x,yPos, RollSetup.transform.position.z);
+            OrientationCross.transform.position =  new Vector3(OrientationCross.transform.position.x,yPos, OrientationCross.transform.position.z);
+        }
+    }
+}
