3cdae0adff23ad02acd4e7fdcd937fe8fc9a093b
Nora <noora.maleki@gmail.com>
2020-10-07 23:19:39 +0200

    Added data frame scripts

---
+++ Assets/Scripts/Functionality/RandomBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class RandomBlock
+{
+   public List<GridElement> LargeGridClose;
+   public List<GridElement> LargeGridFar;
+   public List<GridElement> SmallGrid;
+   public List<GridElement> SmoothPursuit;
+   public List<float> Blink;
+   public List<Luminance> PupilDialation;
+   public List<HeadMovement> Roll;
+   public List<HeadMovement> Yaw;
+   public List<HeadMovement> Pitch;
+   public int FreeViewingindex;
+   public int SmoothPursuitIndex;
+}
--- Assets/Scripts/Functionality/RandomGenerator.cs
+++ Assets/Scripts/Functionality/RandomGenerator.cs
@@ -5,65 +5,122 @@ using UnityEngine;
 using System;
 using Random = System.Random;
 
-// using Random = UnityEngine.Random;
-
 public class RandomGenerator : MonoBehaviour
 {
     [SerializeField] private GameObject fixationPoint;
-
-    float m_MaxDistance;
-    float m_Speed;
-
-    Collider m_Collider;
+    private int _counter = 1;
+    private List<RandomBlock> _Blocks;
     
-    
-    void Start()
+    private void Start()
     {
-        //Choose the distance the Box can reach to
-        m_MaxDistance = 300.0f;
+        _Blocks = new List<RandomBlock>();
+
+        _Blocks.Add(GenerateBlocks()); 
+        
+        /*for (int i = 0; i < 6; i++)
+        {
+            _Blocks.Add(GenerateBlocks());   
+        }*/
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
             GetNextPosition();
-            Debug.Log("Clicked");
         }
     }
 
-    void GetNextPosition()
+    RandomBlock GenerateBlocks()
     {
-        Debug.Log("here");
+        RandomBlock block = new RandomBlock();
+
+        block.LargeGridClose = GetPositionList(24);
+        /*block.LargeGridFar = GetPositionList(24);
+        block.SmallGrid = GetPositionList(12);
+        block.SmoothPursuit = GetPositionList(24);*/
+
+        foreach (var item in block.LargeGridClose)
+        {
+            Debug.Log(item.Position);
+        }
         
+        // TODO fill out the rest
+
+        return block;
+    }
+
+    private List<GridElement> GetPositionList(int numberOfElements)
+    {
+        List<GridElement> gridElements = new List<GridElement>();
+
+        for (int i = 0; i < numberOfElements; i++)
+        {
+            // Debug.Log(i);
+            GridElement grid = new GridElement {Position = GetNextPosition()};
+
+            // TODO 
+            /*grid.FixationDuration
+            grid.MovementDuration*/
+            if (grid.Position != Vector3.zero)
+            {
+                gridElements.Add(grid);
+            }
+            else
+            {
+                i--;
+            }
+        }
+        
+        return gridElements;
+    }
+
+    private Vector3 GetNextPosition()
+    {
+        List<RaycastHit> hitList = GetHitList();
+        Random rand = new Random();
+        int index = rand.Next(hitList.Count);
+        
+        if (hitList.Any())
+        {
+            _counter = 1;
+            
+            fixationPoint.transform.position = hitList[index].collider.transform.position;
+            hitList[index].collider.gameObject.SetActive(false);
+            return fixationPoint.transform.position;
+        }
+
+        if (_counter <= 5)
+        {
+            _counter++;
+            GetNextPosition();
+        }
+
+        return default;
+    }
+    
+    private List<RaycastHit> GetHitList()
+    {
         RaycastHit[] hits;
         List<RaycastHit> hitList = new List<RaycastHit>();
-        Vector3 halfExtents = new Vector3(.2f, .13f, .07f);
-        
-        //Test to see if there is a hit using a BoxCast
-        //Calculate using the center of the GameObject's Collider(could also just use the GameObject's position), half the GameObject's size, the direction, the GameObject's rotation, and the maximum distance as variables.
-        //Also fetch the hit data
-        
-        hits = Physics.BoxCastAll(fixationPoint.transform.position, halfExtents, fixationPoint.transform.forward, fixationPoint.transform.rotation, 100f);
+        Vector3 halfExtents = new Vector3(.2f * _counter, .13f * _counter, .07f);
+        // Debug.Log("halfExtents: " + halfExtents);
 
-
+        hits = Physics.BoxCastAll(fixationPoint.transform.position, halfExtents, 
+            fixationPoint.transform.forward, fixationPoint.transform.rotation, 300f);
+        
         foreach (var hit in hits)
         {
-            if (hit.collider.name != "FixationPoint" || hit.collider.name != "LargeGrid (1)")
-
+            if (hit.collider.name != "FixationPoint" && hit.collider.name != "LargeGrid(1)" 
+                                                     && hit.collider.name != "LargeGrid(2)"
+                                                     && hit.collider.name != "SmallGrid")
             {
                 hitList.Add(hit);
             }
         }
         
-        if (hitList.Any())
-        {
-            Random rand = new Random();
-            int index = rand.Next(hitList.Count);
-            
-            fixationPoint.transform.position = hitList[index].collider.transform.position;
-            hitList[index].collider.gameObject.SetActive(false);
-        }
+        return hitList;
     }
+
+    
 }
