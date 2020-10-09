650e66f91c8e9248866e078a3439e65f5e36f867
Nora <noora.maleki@gmail.com>
2020-10-09 14:18:52 +0200

    Added the new functions

--- Assets/Scripts/Functionality/RouteGenerator.cs
+++ Assets/Scripts/Functionality/RouteGenerator.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using Random = System.Random;
+using RandomUnity = UnityEngine.Random;
+
+
 public class RouteGenerator : MonoBehaviour
 {
-  private GameObject _fixationPoint;
+    private GameObject _fixationPoint;
     private Random _random;
-    private bool _isDone;
-    private List<GridElement> _gridRoute;
     
-    private List <List<GridElement>> _validGridRoutes;
+    private bool _isDone;
+    private bool _isSmoothPursuit;
+
+    private List<GridElement> _gridRoute;
+    private List <List<GridElement>> _validGridRoutes; // TODo maybe in another class?
 
     private bool _inValid;
     
@@ -22,16 +27,14 @@ public class RouteGenerator : MonoBehaviour
         _fixationPoint = ExperimentManager.Instance.GetFixationPoint();
     }
 
-
     
-
-    private List<RaycastHit> GetHitList(Vector3 position, float x=.2f, float y=.13f)
+    private List<RaycastHit> GetHitList(float x=.2f, float y=.13f)
     {
         List<RaycastHit> hitList = new List<RaycastHit>();
         
         Vector3 halfExtents = new Vector3(x, y, .07f);
 
-        var hits = Physics.BoxCastAll(position, halfExtents, 
+        var hits = Physics.BoxCastAll(_fixationPoint.transform.position, halfExtents, 
             _fixationPoint.transform.forward, _fixationPoint.transform.rotation, 300f);
         
         foreach (var hit in hits)
@@ -47,56 +50,6 @@ public class RouteGenerator : MonoBehaviour
         return hitList;
     }
 
-
-    /*IEnumerator Traverse(GameObject grid)
-    {
-        int count = grid.transform.childCount;
-
-        while (count != 0)
-        {
-            List<RaycastHit> hitList= GetHitList(_fixationPoint.transform.position);
-
-            if (!hitList.Any())
-            {
-                Debug.Log("I'm empty");
-                for (int i = 2; i < 5; i++)
-                {
-                    Debug.Log("i: " + i);
-
-                    hitList= GetHitList(_fixationPoint.transform.position,.2f*i, .13f*i);
-                    if (hitList.Any())
-                    {
-                        break;
-                    }
-                }
-
-                // break;
-            }
-            
-            int index = _random.Next(hitList.Count);
-            Vector3 newPosition = hitList[index].collider.transform.position;
-            _fixationPoint.transform.position = newPosition;
-            GridElement gridElement = new GridElement 
-            {ObjectName = hitList[index].collider.name,
-                Position = newPosition};
-            
-            hitList[index].collider.gameObject.SetActive(false);
-            _gridElements.Add(gridElement);
-
-            count--;
-        }
-        
-        foreach (var VARIABLE in _gridElements)
-        {
-            Debug.Log(VARIABLE.ObjectName);
-            Debug.Log(VARIABLE.Position);
-        }
-        
-        Debug.Log(_gridElements.Count);
-        
-        yield return null;
-    }*/
-    
     private void Traverse(GameObject grid)
     {
         int count = grid.transform.childCount;
@@ -107,7 +60,7 @@ public class RouteGenerator : MonoBehaviour
         _inValid = false;
         while (count > 0)
         {
-            List<RaycastHit> hitList = GetHitList(_fixationPoint.transform.position);
+            List<RaycastHit> hitList = GetHitList();
             Debug.Log("count " + count );
 
             if (!hitList.Any())
@@ -117,7 +70,7 @@ public class RouteGenerator : MonoBehaviour
                 {
                     Debug.Log("<color=blue> c:" + count + "i: " + i + "</color>");
 
-                    hitList= GetHitList(_fixationPoint.transform.position,.2f*i, .13f*i);
+                    hitList= GetHitList(.2f*i, .13f*i);
 
                     if (hitList.Any())
                     {
@@ -129,14 +82,12 @@ public class RouteGenerator : MonoBehaviour
                         _inValid = true;
                     }
                 }
-
-               
             }
 
             
             if(!_inValid)
             {
-                 oldPos = _fixationPoint.transform.position;
+                oldPos = _fixationPoint.transform.position;
                 int index = _random.Next(hitList.Count);
                 Vector3 newPosition = hitList[index].collider.transform.position;
                 _fixationPoint.transform.position = newPosition;
@@ -224,21 +175,9 @@ public class RouteGenerator : MonoBehaviour
         {
             Debug.Log("needs to be rerouted"); 
         }
-        
-        
-        
     }
     
-    /*private IEnumerator GetElementList(GameObject grid)
-    {
-        yield return Traverse(grid);
-    }
     
-    private List<GridElement> GetGridElements(GameObject grid)
-    {
-        StartCoroutine(GetElementList(grid));
-        return _gridElements;
-    }*/
     private void CheckWithOtherRoutes(List<GridElement> route )
     {
         if (!_validGridRoutes.Any())
@@ -260,5 +199,17 @@ public class RouteGenerator : MonoBehaviour
         {
             Debug.Log("invalid");
         }
+    }
+    
+    private float GenerateRandomFixationTime()
+    {
+        if (_isSmoothPursuit) return 1;
+        else
+            return (RandomUnity.value <= 0.5) ? 1 : 1.5f;
+    }
+    
+    private float GenerateMovementTime()
+    {
+        return (_isSmoothPursuit) ? 2 : 0;
     }
 }
