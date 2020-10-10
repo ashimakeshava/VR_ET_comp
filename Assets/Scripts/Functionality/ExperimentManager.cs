ed3d3de5be81567b3c7b8a6bf34b5acdbad079af
Nora <noora.maleki@gmail.com>
2020-10-10 21:24:24 +0200

    Implemented the experiment structure

--- Assets/Scripts/Functionality/ExperimentManager.cs
+++ Assets/Scripts/Functionality/ExperimentManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using System;
+using UnityEngine.UI;
 using Random = System.Random;
 
 public class ExperimentManager : MonoBehaviour
@@ -11,21 +12,31 @@ public class ExperimentManager : MonoBehaviour
 
     public static ExperimentManager Instance { get ; private set; } 
     
+    [Header("Grids and Fixation Point")] [Space]
     [SerializeField] private GameObject fixationPoint;
-    [SerializeField] private GameObject largeGrid1;
-    [SerializeField] private GameObject largeGrid2;
-    [SerializeField] private GameObject smallGrid;
+    [SerializeField] private GameObject largeGrid;
+    [SerializeField] private GameObject smallGrid;    // todo remove
+    
+    [Header("Constant Trials between all Participants")] [Space]
     [SerializeField] private List<GameObject> freeViewingPictures;
+    [SerializeField] private List<Text> trialInstructions;
+    
+    [Header("Texts")] [Space]
+    [SerializeField] private Text welcome;    // todo write the welcome message and give instruction for calibration
+    [SerializeField] private Text blockEnd;    // todo write the block ended message
+    [SerializeField] private Text afterBlockThree;    // todo write the force break message
+    [SerializeField] private Text thankYou;    // todo write the experiment ended message
 
     private List<List<GridElement>> _smoothPursuitRoutes;
-    private List<List<GridElement>> _randimizedSmoothPursuitRoutes;
+    private List<List<GridElement>> _randomizedSmoothPursuitRoutes;
     private List<GameObject> _randomizedPictureList;
     private List<Block> _blocks;
 
     private Random _random;
-    private bool _continueTrials;
-    private bool _experimentIsRunning;
-    private int _blockCounter;
+    private bool _continue;
+    private bool _trialIsRunning;
+    private int _blockIndex;
+    private int _trialIndex;
 
     #endregion
 
@@ -38,39 +49,73 @@ public class ExperimentManager : MonoBehaviour
         {
             Instance = this;
         }
+        
+        // todo read the corresponding file or make one if !exists
     }
 
     private void Start()
     {
         _random = new Random();
-
-        // todo read all of the randomization lists from the list
-        _smoothPursuitRoutes = new List<List<GridElement>>();    // todo read the routes from file
-        
-        _randomizedPictureList = RandomizeFreeViewingPictures();
-        _randimizedSmoothPursuitRoutes = RandomizeSmoothPursuitSequence();
-        
         _blocks = new List<Block>();
 
+        // todo read all of the randomization lists from the list
+        _smoothPursuitRoutes = new List<List<GridElement>>();    // todo get routes from file
+        
+        _randomizedPictureList = RandomizeFreeViewingPictures();
+        _randomizedSmoothPursuitRoutes = RandomizeSmoothPursuitSequence();
+        
+        
         for (int i = 0; i < 6; i++)
         {
             _blocks.Add(GetComponent<BlockGenerator>().GenerateBlock(_randomizedPictureList[i], _smoothPursuitRoutes[i]));
+            // todo save the data
         }
         
-        // todo show instruction and welcome message
+        welcome.gameObject.SetActive(true);
     }
 
     private void Update()
     {
-        // TODO click or push trigger to start the experiment
-
-        if (!_experimentIsRunning)
+        // todo click or push trigger to continue with the experiment
+        if (!_trialIsRunning)
         {
+            ResetFixationPoint();
+            
+            if (_blockIndex == 6)
+            {
+                thankYou.gameObject.SetActive(true);
+            }
+            else if (_trialIndex == 12)
+            {
+                // todo save data
+                if (_blockIndex == 3) afterBlockThree.gameObject.SetActive(true);
+                else blockEnd.gameObject.SetActive(true);
+
+                _blockIndex++;
+                _trialIndex = 0;
+
+                if (_continue)
+                {
+                    _continue = false;
+                    afterBlockThree.gameObject.SetActive(false);
+                    blockEnd.gameObject.SetActive(false);
+                    TrialInstructionActivation(true);
+                    if (_continue) ExecuteTrials();
+                }
+            }
+            else
+            {
+                TrialInstructionActivation(true);
+                if (_continue) ExecuteTrials();
+            }
+            
             if (Input.GetKeyDown(KeyCode.Return))
             {
-                _continueTrials = true;
+                _continue = true;
             }
         }
+
+        
 
         #region DebugingPurpose
 
@@ -89,92 +134,68 @@ public class ExperimentManager : MonoBehaviour
             fixationPoint.transform.position= new Vector3(0,0,1);
             Debug.Log("___________________________________-----_____________________________");
         }
-        
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            StartExperiment();
-        }
-        
+
         #endregion
     }
 
-
-    void StartExperiment()
+    private void ResetFixationPoint()
     {
-        #region Done
-        // creat block counter
-
-        
-
-        #endregion
-        
-        // block 0.seq 0 (read the trial)
-        // write it to the participant progress file // todo??? blocks + seq
-        // calibrate
-        // show trial instruction // todo a canvas with different instructions
-        // wait for click
-        // execute trial
-        // experiment is running == true
-        // done?
-            // if yes ? remove seq 0 && exp is running = false : repeat trial 
-        // if block 0.seq == empty ? remove block 0
-            // counter++
-            // show take a break
-            // wait for click
-            // calibrate
-        // if counter 3 -> force break
-        // if blocks == empty ? show thank you text
-        // repeat
-            
-        // todo iterate through blocks   
-        foreach (var block in _blocks)
-        {
-            foreach (var value in block.SequenceOfTrials)
-            {
-                _continueTrials = false;
-                ExecuteTrials(value, block);
-                while (!_continueTrials) {}
-            }
-        }
+        fixationPoint.transform.position = new Vector3(0, 0, 1);
+        fixationPoint.gameObject.SetActive(false);
     }
 
+
+    private void TrialInstructionActivation(bool activate)
+    {
+        welcome.gameObject.SetActive(false);
+        trialInstructions[_blocks[_blockIndex].SequenceOfTrials[_trialIndex]].gameObject.SetActive(activate);
+    }
     
-    // todo implement 
-    void ExecuteTrials(int value, Block block)
+    
+    void ExecuteTrials()    // todo implement 
     {
-        switch (value)
+        _trialIsRunning = true;
+        _continue = false;
+        TrialInstructionActivation(false);
+        
+        switch (_blocks[_blockIndex].SequenceOfTrials[_trialIndex])
         {
-            case 1:
-                GetComponent<Validation>().StartValidation(block.LargeGridClose, block.LargeGridFar);
+            case 0:    // calibration
+                // calibration
                 break;
-            case 2:
+            case 1:    // Validation
+                GetComponent<Validation>().StartValidation(_blocks[_blockIndex].LargeGridClose, _blocks[_blockIndex].LargeGridFar);
+                break;
+            case 2:    // Smooth pursuit
                 
                 break;
-            case 3:
+            case 3:    // Small grid
                 
                 break;
-            case 4:
+            case 4:    // Blink
                 
                 break;
-            case 5:
+            case 5:    // Pupil dilation
                 
                 break;
-            case 6:
+            case 6:    // Free viewing
                 
                 break;
-            case 7:
+            case 7:    // Roll
                 
                 break;
-            case 8:
+            case 8:    // Yaw
                 
                 break;
-            case 9:
+            case 9:    // Pitch
                 
                 break;
-            case 10:
-                
+            case 10:    // Micro saccades
+                GetComponent<MicroSaccades>().RunMicroSaccades();
                 break;
         }
+
+        _trialIndex++;
     }
 
     private List<GameObject> RandomizeFreeViewingPictures()
@@ -204,11 +225,6 @@ public class ExperimentManager : MonoBehaviour
         
         return list;
     }
-    
-    private void ShowInstructionForNextTrial()
-    {
-        throw new NotImplementedException();
-    }
 
     #endregion
     
@@ -216,7 +232,7 @@ public class ExperimentManager : MonoBehaviour
 
     public void TrialEnded()
     {
-        ShowInstructionForNextTrial();
+        _trialIsRunning = false;
     }
 
     public GameObject GetFixationPoint()
@@ -224,15 +240,11 @@ public class ExperimentManager : MonoBehaviour
         return fixationPoint;
     }
     
-    public GameObject GetLargeGrid1()
+    public GameObject GetLargeGrid()
     {
-        return largeGrid1;
+        return largeGrid;
     }
     
-    public GameObject GetLargeGrid2()
-    {
-        return largeGrid2;
-    }
     public GameObject GetSmallGrid()
     {
         return smallGrid;
