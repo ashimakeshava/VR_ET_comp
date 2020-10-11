4ad14cb73c585135c705d64e5fd8e2dffd4fb9d7
Nora <noora.maleki@gmail.com>
2020-10-11 20:14:44 +0200

    Refined scripts

--- Assets/Scripts/Trials/SmoothPursuit.cs
+++ Assets/Scripts/Trials/SmoothPursuit.cs
@@ -22,36 +22,27 @@ public class SmoothPursuit : MonoBehaviour
         _grid = ExperimentManager.Instance.GetLargeGrid();    // todo change it to only grid after serialization is done
     }
 
-    IEnumerator StartSmallGrid()
+    IEnumerator StartSmoothPursuit()
     {
         _grid.gameObject.SetActive(true);
         _fixationPoint.gameObject.SetActive(true);
         
         yield return new WaitForSeconds((RandomUnity.value <= 0.5) ? 1 : 1.5f);    // todo generate this time as well or save this time
 
-        for (int i = 0; i < _smoothPursuit.Count; i++)
+        for (int i = 0; i < _smoothPursuit.Count - 1; i++)
         {
             _t += Time.deltaTime / _timeToReachGoal;
             _fixationPoint.transform.position = Vector3.Lerp(_smoothPursuit[i].Position, _smoothPursuit[i + 1].Position, _t);
             
             yield return new WaitForSeconds(_smoothPursuit[i].FixationDuration);
         }
-        
-        foreach (var element in _smoothPursuit)
-        {
-            
-            
-            _fixationPoint.transform.position = element.Position;
 
-            
-        }
-        
         ExperimentManager.Instance.TrialEnded();
     }
 
-    public void RunSmallGrid(List<GridElement> smallGrid)
+    public void RunSmoothPursuit(List<GridElement> smallGrid)
     {
         _smoothPursuit = smallGrid;
-        StartCoroutine(StartSmallGrid());
+        StartCoroutine(StartSmoothPursuit());
     }
 }
