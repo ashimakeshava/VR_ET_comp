cb61e98f77d5abe8f86286f53b208d5d15798009
Nora <noora.maleki@gmail.com>
2020-10-11 21:25:26 +0200

    Refinement

--- Assets/Scripts/Trials/SmoothPursuit.cs
+++ Assets/Scripts/Trials/SmoothPursuit.cs
@@ -29,7 +29,7 @@ public class SmoothPursuit : MonoBehaviour
         
         yield return new WaitForSeconds((RandomUnity.value <= 0.5) ? 1 : 1.5f);    // todo generate this time as well or save this time
 
-        for (int i = 0; i < _smoothPursuit.Count - 1; i++)
+        for (int i = 0; i <= _smoothPursuit.Count; i++)
         {
             _t += Time.deltaTime / _timeToReachGoal;
             _fixationPoint.transform.position = Vector3.Lerp(_smoothPursuit[i].Position, _smoothPursuit[i + 1].Position, _t);
