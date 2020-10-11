661235bb9601f19c241059505dcd2e6cf8363362
Nora <noora.maleki@gmail.com>
2020-10-11 15:12:46 +0200

    Cleaned up the script

--- Assets/Scripts/Trials/Validation.cs
+++ Assets/Scripts/Trials/Validation.cs
@@ -6,7 +6,7 @@ using RandomUnity = UnityEngine.Random;
 public class Validation : MonoBehaviour
 {
     private GameObject _fixationPoint;
-    private GameObject _gridClose;
+    private GameObject _grid;
     
     private List<GridElement> _gridElementsClose;
     private List<GridElement> _gridElementsFar;
@@ -15,12 +15,12 @@ public class Validation : MonoBehaviour
     void Start()
     {
         _fixationPoint = ExperimentManager.Instance.GetFixationPoint();
-        _gridClose = ExperimentManager.Instance.GetLargeGrid();
+        _grid = ExperimentManager.Instance.GetLargeGrid();
     }
 
     IEnumerator StartFirstValidation()
     {
-        _gridClose.gameObject.SetActive(true);
+        _grid.gameObject.SetActive(true);
         _fixationPoint.gameObject.SetActive(true);
         yield return new WaitForSeconds((RandomUnity.value <= 0.5) ? 1 : 1.5f);    // todo save this time
 
@@ -31,7 +31,7 @@ public class Validation : MonoBehaviour
            yield return new WaitForSeconds(element.FixationDuration);
         }
         
-        _gridClose.gameObject.transform.position = new Vector3(0, 0, 2);
+        _grid.gameObject.transform.position = new Vector3(0, 0, 2);
         StartCoroutine(StartSecondValidation());
     }
     
@@ -47,7 +47,7 @@ public class Validation : MonoBehaviour
             yield return new WaitForSeconds(element.FixationDuration);
         }
 
-        _gridClose.gameObject.transform.position = new Vector3(0, 0, 1);
+        _grid.gameObject.transform.position = new Vector3(0, 0, 1);
         ExperimentManager.Instance.TrialEnded();
     }
 
