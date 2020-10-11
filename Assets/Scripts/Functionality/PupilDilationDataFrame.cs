c825c31b077c6b9292ce5a876472c66df35c8567
Nora <noora.maleki@gmail.com>
2020-10-12 00:48:52 +0200

    Created pupil dilation data frame

---
+++ Assets/Scripts/Functionality/PupilDilationDataFrame.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+
+[Serializable]public class PupilDilationDataFrame
+{
+    public int ColorIndex;
+    public float ColorDuration;
+}
