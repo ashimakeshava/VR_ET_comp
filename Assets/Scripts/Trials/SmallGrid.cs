19f5b84ae4a68cd7524db13a3b21c64a114e8763
Nora <noora.maleki@gmail.com>
2020-10-11 15:12:29 +0200

    Implemented small grid movement

---
+++ Assets/Scripts/Trials/SmallGrid.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RandomUnity = UnityEngine.Random;
+
+public class SmallGrid : MonoBehaviour
+{
+    private GameObject _fixationPoint;
+    private GameObject _grid;
+    
+    private List<GridElement> _smallGrid;
+    
+
+    void Start()
+    {
+        _fixationPoint = ExperimentManager.Instance.GetFixationPoint();
+        _grid = ExperimentManager.Instance.GetLargeGrid();
+    }
+
+    IEnumerator StartFirstValidation()
+    {
+        _grid.gameObject.SetActive(true);
+        _fixationPoint.gameObject.SetActive(true);
+        yield return new WaitForSeconds((RandomUnity.value <= 0.5) ? 1 : 1.5f);    // todo generate this time as well or save this time
+
+        foreach (var element in _smallGrid)
+        {
+            _fixationPoint.transform.position = element.Position;
+
+            yield return new WaitForSeconds(element.FixationDuration);
+        }
+        
+        _grid.gameObject.transform.position = new Vector3(0, 0, 2);
+        ExperimentManager.Instance.TrialEnded();
+    }
+
+    public void StartValidation(List<GridElement> smallGrid)
+    {
+        _smallGrid = smallGrid;
+        StartCoroutine(StartFirstValidation());
+    }
+}
