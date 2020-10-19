37eb46063e8a1fc94ca0796b9d24f5f75b6d18df
Nora <noora.maleki@gmail.com>
2020-10-20 00:45:11 +0200

    Created cross elements to pass on the relevant object

---
+++ Assets/Scripts/Headtracking/CrossElement.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrossElement : MonoBehaviour
+{
+    public GameObject targetCrossElement;
+    public bool correctAligned;
+}
