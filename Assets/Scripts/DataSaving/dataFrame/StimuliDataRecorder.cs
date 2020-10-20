4813668ccdbb41ab927481ae49568fd2a00a1754
Linus <linus-tiemann@live.de>
2020-10-20 18:17:17 +0100

    stimuli Data Recorder

---
+++ Assets/Scripts/DataSaving/dataFrame/StimuliDataRecorder.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class StimuliDataRecorder : MonoBehaviour
+{
+    private bool runningRecording;
+    
+    private void StartRecording()
+    {
+        StartCoroutine(RecordStimuliEvents);
+        runningRecording = true;
+    }
+
+    public IEnumerator RecordStimuliEvents()
+    {
+        while (runningRecording)
+        {
+            StimuliDataFrame data = new StimuliDataFrame();
+
+            data.UnixTimeStamp = TimeManager.Instance.GetCurrentUnixTimeStamp();
+
+            data.StimuliOnset = ExperimentManager.Instance.GetStimuliOnset()
+        }
+    }
+    
+    
+    
+    public 
+    
+}
