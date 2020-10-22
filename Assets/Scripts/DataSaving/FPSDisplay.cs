0188f9caad4e19e43c8b4e1d0b7b71f5b6a83376
Nora <noora.maleki@gmail.com>
2020-10-22 03:52:02 +0200

    Changed the color of the FPS display

--- Assets/Scripts/DataSaving/FPSDisplay.cs
+++ Assets/Scripts/DataSaving/FPSDisplay.cs
@@ -25,7 +25,7 @@ public class FPSDisplay : MonoBehaviour
         Rect rect = new Rect(0, 0, w, h * 2 / 100);
         style.alignment = TextAnchor.UpperLeft;
         style.fontSize = h * 2 / 100;
-        style.normal.textColor = Color.cyan;
+        style.normal.textColor = Color.black;
         float msec = _deltaTime * 1000.0f;
         float fps = 1.0f / _deltaTime;
         string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
